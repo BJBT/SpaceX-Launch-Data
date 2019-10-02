bdf197eefea452902d3533ab30c3a3c57f84d6e4
Blayne Bennett <blaynebennett99@gmail.com>
2019-10-02 13:53:03 -0700

    finished app; deploy to heroku

---
+++ public/precache-manifest.240c41142abe8750da721f44625822bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a80b02e4306939babdbf2605763ed44b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56b2de14eb3a67181a42",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "1bd1b30ac420334799f1",
+    "url": "/static/js/2.cc900aa3.chunk.js"
+  },
+  {
+    "revision": "56b2de14eb3a67181a42",
+    "url": "/static/js/main.061cb2e6.chunk.js"
+  },
+  {
+    "revision": "2055ac218fe3f7ad47ca",
+    "url": "/static/js/runtime-main.a7ca617d.js"
+  },
+  {
+    "revision": "01f343224f938e67b9a5aa6146ac133d",
+    "url": "/static/media/spacex.01f34322.png"
+  }
+]);
